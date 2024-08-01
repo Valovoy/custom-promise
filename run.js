bcfc0cd0a6855b3eb58fcc4028e82d508a8e7af6
Valovoy <aleksandr.valovoy1@gmail.com>
2024-08-01 21:48:53 +0300

    update onRejected

--- run.js
+++ run.js
@@ -1,26 +1,62 @@
 class CustomPromise {
 	#state = 'pending'
 	#result = undefined
-	#hendlers = []
+	#handlers = []
 
 	constructor(executor) {
-		executor(this.resolve)
+		this.resolve = this.resolve.bind(this)
+		this.reject = this.reject.bind(this)
+
+		try {
+			executor(this.resolve, this.reject)
+		} catch (error) {
+			this.reject(error)
+		}
 	}
 
-	resolve = result => {
-		this.#state = 'fulfilled'
+	#onHandler(state, result) {
+		this.#state = state
 		this.#result = result
 
-		this.#hendlers.forEach(func => func(this.#result))
+		this.#handlers.forEach(func => func(this.#result))
+		this.#handlers = []
 	}
 
-	then(func) {
-		if (this.#result) {
-			func(this.#result)
-		} else {
-			this.#hendlers.push(func)
-		}
+	resolve(result) {
+		this.#onHandler('fulfilled', result)
+	}
 
-		return this
+	reject(error) {
+		this.#onHandler('rejected', error)
+	}
+
+	then(onFulfilled, onRejected) {
+		return new CustomPromise((resolve, reject) => {
+			const handler = () => {
+				try {
+					if (this.#state === 'fulfilled') {
+						const result = onFulfilled
+							? onFulfilled(this.#result)
+							: this.#result
+						resolve(result)
+					} else {
+						if (onRejected) {
+							const result = onRejected(this.#result)
+							resolve(result)
+						} else {
+							reject(this.#result)
+						}
+					}
+				} catch (error) {
+					reject(error)
+				}
+			}
+
+			if (this.#state === 'pending') {
+				this.#handlers.push(handler)
+			} else {
+				handler()
+			}
+		})
 	}
 }
